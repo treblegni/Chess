c84f57243023fd900afab2e09244808b5da311c7
treblegni <ingelbert.figueroa@gmail.com>
2020-07-20 11:29:46 -0700

    connected redux

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Board from '../components/board';
 
 function App() {
-  return (
-    <div className="App">
-      <Board></Board>
-    </div>
-  );
+  	return (
+		<div className="App">
+			<Board></Board>
+		</div>
+  	);
 }
 
 export default App;
--- src/components/tile/index.js
+++ src/components/tile/index.js
@@ -1,21 +1,28 @@
 import React,{ useState } from 'react';
+import { compose } from 'recompose';
+import { connect } from 'react-redux';
 
 const Tile = (props) =>  {
-    const [tile,setTile] = useState({isSelected: false, style: {}, isOccupied: false});
+    const [style,setTile] = useState({});
 
     const handleSelect = () => {
-        console.log(tile.isSelected);
-        setTile({...tile,isSelected: !tile.isSelected});
-        console.log(tile.isSelected);
-
-        if (tile.isSelected) setTile({...tile, style: {borderColor: 'red'}});
-        else setTile({...tile, style: {borderColor: 'black'}});
     }
 
     return (
-        <div {...props} style={tile.style} onClick={handleSelect}>
+        <div {...props} style={style} onClick={handleSelect}>
         </div>
     )
 }
 
-export default Tile;
+const enhance = compose(
+    connect(
+        state => ({
+            
+        }),
+        dispatch => ({
+
+        })
+    )
+)
+
+export default enhance(Tile);
